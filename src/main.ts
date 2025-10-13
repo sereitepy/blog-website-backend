29e1d72d779afe30e5a2a09b04343753c6a21c3d
sereitepy <106307116+sereitepy@users.noreply.github.com>
2025-10-13 18:53:03 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
+    // according to the doc, this origin, will allow only the requests that comes from the defined route
+    credentials: true, // we set this to true to 'enable' CORS so that, the request only work with the defined route. if set to false, that means we 'disable' CORS.
   });
 
   const port = process.env.PORT || 3001;
